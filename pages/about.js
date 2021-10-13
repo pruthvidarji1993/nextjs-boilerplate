9fc663049ef44977e0e41d4103c6d7cdbbb5a5d8
pruthvidarji1993 <77260256+pruthvidarji1993@users.noreply.github.com>
2021-10-13 16:53:13 +0530

    initial commit for next js boilerplate to use of initial start of the project

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Sample Code</h1>
+      <p>Application Created by PruthviZignuts</p>
+    </div>
+  </Layout>
+);
+
+export default About;
